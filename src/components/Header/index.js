c54ef5f8c9e0e1835a53bb475ca1914e55dc1c4e
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-04-20 13:52:26 -0300

    Add size information to cart

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { MdShoppingBasket } from 'react-icons/md';
 
@@ -6,7 +7,7 @@ import { Container, Cart } from './styles';
 
 import logo from '../../assets/images/logo.svg';
 
-export default function Header() {
+function Header({ cartSize }) {
   return (
     <Container>
       <Link to="/">
@@ -16,10 +17,15 @@ export default function Header() {
       <Cart to="/cart">
         <div>
           <strong>Meu Carrinho</strong>
-          <span>3 Itens</span>
+          <span>{cartSize} Itens</span>
         </div>
         <MdShoppingBasket size={36} color="#FFF" />
       </Cart>
     </Container>
   );
 }
+
+export default connect((state) => ({
+  // State.cart is the reducer name
+  cartSize: state.cart.length,
+}))(Header);
