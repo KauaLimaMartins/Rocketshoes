ff135dc4c268231c14baa3fb24c69e52629599db
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-04-20 11:50:12 -0300

    import Provider from react-redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import Routes from './routes';
 import GlobalStyles from './styles/global';
 
 import Header from './components/Header';
 
+import store from './store';
+
 export default function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyles />
-      <Header />
-      <Routes />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalStyles />
+        <Header />
+        <Routes />
+      </BrowserRouter>
+    </Provider>
   );
 }
