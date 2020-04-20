8db9fcc44bf4b87cc2cf5066606cba1b42f87240
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-04-20 13:53:45 -0300

    Integrate api with cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 import {
   MdRemoveCircleOutline,
   MdAddCircleOutline,
@@ -7,38 +8,35 @@ import {
 
 import { Container, ProductTable, Total } from './styles';
 
-export default class Cart extends Component {
-  render() {
-    return (
-      <Container>
-        <ProductTable>
-          <thead>
-            <tr>
-              <th />
-              <th>PRODUTO</th>
-              <th>QTD</th>
-              <th>SUBTOTAL</th>
-              <th />
-            </tr>
-          </thead>
-          <tbody>
+function Cart({ cart }) {
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          {cart.map((product) => (
             <tr>
               <td>
-                <img
-                  src="https://static.netshoes.com.br/produtos/tenis-kappa-impact-masculino/04/D24-1738-304/D24-1738-304_zoom2.jpg?ts=1587184157&ims=326x"
-                  alt="Kappa Impact"
-                />
+                <img src={product.image} alt={product.title} />
               </td>
               <td>
-                <strong>Tênis Muito Legal</strong>
-                <span>R$350,00</span>
+                <strong>{product.title}</strong>
+                <span>{product.priceFormatted}</span>
               </td>
               <td>
                 <div>
                   <button type="button">
                     <MdRemoveCircleOutline size={20} color="#7159c1" />
                   </button>
-                  <input type="number" readOnly value={2} />
+                  <input type="number" readOnly value={product.amount} />
                   <button type="button">
                     <MdAddCircleOutline size={20} color="#7159c1" />
                   </button>
@@ -53,18 +51,24 @@ export default class Cart extends Component {
                 </button>
               </td>
             </tr>
-          </tbody>
-        </ProductTable>
+          ))}
+        </tbody>
+      </ProductTable>
 
-        <footer>
-          <button type="button">Finalizar Pedido</button>
+      <footer>
+        <button type="button">Finalizar Pedido</button>
 
-          <Total>
-            <span>TOTAL</span>
-            <strong>R$1025,00</strong>
-          </Total>
-        </footer>
-      </Container>
-    );
-  }
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1025,00</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
+
+const mapStateToProps = (state) => ({
+  cart: state.cart,
+});
+
+export default connect(mapStateToProps)(Cart);
