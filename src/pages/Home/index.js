470ec595948f21a842a9921345b94e33261f4dde
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-04-20 10:56:04 -0300

    Home page api integration

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import api from '../../services/api';
+import { formatPrice } from '../../utils/format';
+
+import { ProductList } from './styles';
 
 export default class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    const data = response.data.map((product) => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  }
+
   render() {
-    return <div />;
+    const { products } = this.state;
+
+    return (
+      <ProductList>
+        {products.map((product) => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#FFF" />3
+              </div>
+
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
   }
 }
