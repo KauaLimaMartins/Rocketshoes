d6f5b56f95fe8d6510b6ac31946a33252e711d3d
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-04-20 13:51:36 -0300

    import reactotron config

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+import './config/Reactotron';
+
 import Routes from './routes';
 import GlobalStyles from './styles/global';
 
