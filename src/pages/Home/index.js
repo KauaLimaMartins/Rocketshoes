eff544027b2d29da3ba646b72792f00c374f4814
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-04-20 13:54:12 -0300

    Integrate api with home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 
 import api from '../../services/api';
@@ -6,7 +7,7 @@ import { formatPrice } from '../../utils/format';
 
 import { ProductList } from './styles';
 
-export default class Home extends Component {
+class Home extends Component {
   state = {
     products: [],
   };
@@ -22,6 +23,17 @@ export default class Home extends Component {
     this.setState({ products: data });
   }
 
+  handleAddProduct = (product) => {
+    const { dispatch } = this.props;
+
+    dispatch({
+      // All action needs a:
+      type: 'ADD_TO_CART',
+      // The content
+      product,
+    });
+  };
+
   render() {
     const { products } = this.state;
 
@@ -33,9 +45,12 @@ export default class Home extends Component {
             <strong>{product.title}</strong>
             <span>{product.priceFormatted}</span>
 
-            <button type="button">
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct(product)}
+            >
               <div>
-                <MdAddShoppingCart size={16} color="#FFF" />3
+                <MdAddShoppingCart size={16} color="#FFF" /> 3
               </div>
 
               <span>ADICIONAR AO CARRINHO</span>
@@ -46,3 +61,5 @@ export default class Home extends Component {
     );
   }
 }
+
+export default connect()(Home);
