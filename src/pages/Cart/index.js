2d8e2a8d2e8bd43d36ba181b0187c2a8c40ea92c
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-04-20 10:56:57 -0300

    Cart page static informations

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,9 +1,70 @@
 import React, { Component } from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default class Cart extends Component {
   render() {
-    return <h1>Cartinho</h1>;
+    return (
+      <Container>
+        <ProductTable>
+          <thead>
+            <tr>
+              <th />
+              <th>PRODUTO</th>
+              <th>QTD</th>
+              <th>SUBTOTAL</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <img
+                  src="https://static.netshoes.com.br/produtos/tenis-kappa-impact-masculino/04/D24-1738-304/D24-1738-304_zoom2.jpg?ts=1587184157&ims=326x"
+                  alt="Kappa Impact"
+                />
+              </td>
+              <td>
+                <strong>Tênis Muito Legal</strong>
+                <span>R$350,00</span>
+              </td>
+              <td>
+                <div>
+                  <button type="button">
+                    <MdRemoveCircleOutline size={20} color="#7159c1" />
+                  </button>
+                  <input type="number" readOnly value={2} />
+                  <button type="button">
+                    <MdAddCircleOutline size={20} color="#7159c1" />
+                  </button>
+                </div>
+              </td>
+              <td>
+                <strong>R$700,00</strong>
+              </td>
+              <td>
+                <button type="button">
+                  <MdDelete size={20} color="#7159c1" />
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </ProductTable>
+
+        <footer>
+          <button type="button">Finalizar Pedido</button>
+
+          <Total>
+            <span>TOTAL</span>
+            <strong>R$1025,00</strong>
+          </Total>
+        </footer>
+      </Container>
+    );
   }
 }
