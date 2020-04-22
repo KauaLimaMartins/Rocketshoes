5e17452d35b9c731b6054328bd424b60535f2e30
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-04-22 13:52:22 -0300

    Stock validation

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,8 +1,9 @@
 import { call, select, put, all, takeLatest } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import api from '../../../services/api';
 import { formatPrice } from '../../../utils/format';
-import { addToCartSuccess, updateAmount } from './actions';
+import { addToCartSuccess, updateAmountSuccess } from './actions';
 
 function* addToCart({ id }) {
   // select is to catch a reducer
@@ -18,13 +19,13 @@ function* addToCart({ id }) {
   const newAmount = currentAmount + 1;
 
   if (newAmount > stockAmount) {
-    console.tron.warn('ERRO');
+    toast.error('Quantidade solicitada sem estoque');
     return;
   }
 
   if (productExists) {
     // put is to use a action
-    yield put(updateAmount(id, newAmount));
+    yield put(updateAmountSuccess(id, newAmount));
   } else {
     // call is to make a request
     const response = yield call(api.get, `/products/${id}`);
@@ -39,6 +40,23 @@ function* addToCart({ id }) {
   }
 }
 
-// All:
+function* updateAmount({ id, amount }) {
+  if (amount <= 0) return;
+
+  const stock = yield call(api.get, `/stock/${id}`);
+  const stockAmount = stock.data.amount;
+
+  if (amount > stockAmount) {
+    toast.error('Quantidade solicitada sem estoque');
+    return;
+  }
+
+  yield put(updateAmountSuccess(id, amount));
+}
+
+// All: Hear an action and do something when that action is called
 // takeLatest: Use only the last action called
-export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
+export default all([
+  takeLatest('@cart/ADD_REQUEST', addToCart),
+  takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount),
+]);
