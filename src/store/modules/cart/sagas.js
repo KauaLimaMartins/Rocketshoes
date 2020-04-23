b5829c38cd059d372e14c1a4e04e81312892fd02
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-04-23 12:09:50 -0300

    push to cart

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -2,7 +2,9 @@ import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 
 import api from '../../../services/api';
+import history from '../../../services/history';
 import { formatPrice } from '../../../utils/format';
+
 import { addToCartSuccess, updateAmountSuccess } from './actions';
 
 function* addToCart({ id }) {
@@ -37,6 +39,8 @@ function* addToCart({ id }) {
     };
 
     yield put(addToCartSuccess(data));
+
+    history.push('/cart');
   }
 }
 
