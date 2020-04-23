0c0dd68cf9d10529cfad9b8f2d64e3f30563ac49
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-04-23 12:09:23 -0300

    Import history

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 
@@ -10,17 +10,18 @@ import GlobalStyles from './styles/global';
 
 import Header from './components/Header';
 
+import history from './services/history';
 import store from './store';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <Router history={history}>
         <GlobalStyles />
         <Header />
         <Routes />
         <ToastContainer autoClose={3000} />
-      </BrowserRouter>
+      </Router>
     </Provider>
   );
 }
